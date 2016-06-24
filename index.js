0fdd45f2503083174a9369ba46f11b41f9234b93
Rahul Jain <rahul.jain.9684@gmail.com>
2016-06-24 15:27:36 +0530

    Formatted to use tabs

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ var mergeDataSources = exports.mergeDataSources = function(instructions, subAppI
 			instructions.dataSources[dsName] = subAppInstructions.dataSources[dsName];
 			console.log('Adding dataSources from sub app: ',dsName);
 		} else {
-            mergeKeys(instructions.dataSources[dsName], subAppInstructions.dataSources[dsName]);
+			mergeKeys(instructions.dataSources[dsName], subAppInstructions.dataSources[dsName]);
         }
 	});
 };
@@ -165,15 +165,15 @@ function getBaseModelName(modelDefinition) {
 }
 
 function mergeKeys(destination, source) {
-    Object.keys(source).forEach(function (key) {
-        if (!destination.hasOwnProperty(key)) {
-            destination[key] = source[key];
-        } else {
-            if (typeof destination[key] === 'object' && typeof source[key] === 'object') {
-                mergeKeys(destination[key], source[key]);
-            } else {
-                console.warn('could not merge %s for %s. Leaving destination values.', key, dsName);
-            }
-        }
-    });
+	Object.keys(source).forEach(function (key) {
+		if (!destination.hasOwnProperty(key)) {
+			destination[key] = source[key];
+		} else {
+			if (typeof destination[key] === 'object' && typeof source[key] === 'object') {
+				mergeKeys(destination[key], source[key]);
+			} else {
+				console.warn('could not merge %s for %s. Leaving destination values.', key, dsName);
+			}
+		}
+	});
 }
